1842b90da373f02eae4b018e1c3471466f84210d
Daniel Wagner <daniel@wagner-home.com>
2013-11-12 17:51:09 -0500

    improvements to latex spew filter

--- latex.hs
+++ latex.hs
@@ -16,15 +16,16 @@ coalesce = map concat
          . groupWhen (\l -> length l == 79 && not (".tex" `isSuffixOf` l))
          . lines
 
-data InterestLevel = Boring
-	deriving (Eq, Ord, Show, Read, Enum, Bounded)
 data MessageLevel = Info | Warning | Error
 	deriving (Eq, Ord, Show, Read, Enum, Bounded)
 data Line
-	= Known InterestLevel String
+	= Boring String
+	| HBox String String
 	| File String [Line]
 	| Message MessageLevel String [String]
-	-- TODO: add LineMarker Integer and emit an extra Line for each line that matches "line [[:digit:]]+" or something like that
+	-- TODO: add LineMarker Integer and emit an extra Line for each line that
+	-- matches "line [[:digit:]]+" or "lines [[:digit:]]+--[[:digit:]]+" or
+	-- something like that
 	| ExtraCloseFile
 	| Unknown String
 	deriving (Eq, Ord, Show, Read)
@@ -42,12 +43,14 @@ equalities =
 	," restricted \\write18 enabled."
 	," %&-line parsing enabled."
 	,"**paper"
-	,""
 	,"For additional information on amsmath, use the `?' option."
 	,"ABD: EveryShipout initializing macros"
+	,"Here is how much of TeX's memory you used:"
+	,"PDF statistics:"
 	]
 regexen = map compile $
-	["^LaTeX2e <[[:digit:]]{4}/[[:digit:]]{2}/[[:digit:]]{2}>$"
+	["^[[:space:]]*$"
+	,"^LaTeX2e <[[:digit:]]{4}/[[:digit:]]{2}/[[:digit:]]{2}>$"
 	,"^Babel <.*> and hyphenation patterns for [[:digit:]]* languages loaded\\.$"
 	,"^Document Class: .* Standard LaTeX document class$"
 	,"^File: " ++ filenameRegex ++ " [[:digit:]]{4}/[[:digit:]]{2}/[[:digit:]]{2}"
@@ -55,8 +58,28 @@ regexen = map compile $
 	,"^\\\\[^ =]+=\\\\(count|dimen|toks|mathgroup|skip|box|muskip|write|read)[[:digit:]]+$"
 	,"^\\\\openout[[:digit:]]+ = [^']*'\\.$"
 	,"^Chapter [[:digit:]]+\\.$"
-	]
+	,"^[[:space:]]*<" ++ filenameRegex ++ ", id=[[:digit:]]+, " ++ ptRegex ++ " x " ++ ptRegex ++ ">$"
+	,"^File: " ++ filenameRegex ++ " Graphic file \\(type [a-z]+\\)$"
+	,"^<use " ++ filenameRegex ++ ">$"
+	,"^ [[:digit:]]+ (" ++ intercalate "|" statistics ++ ") out of "
+	,"^ [^ ]* stack positions out of"
+	,"^ [[:digit:]]+ compressed objects within [[:digit:]]+ object streams$"
+	,"^([<>]|" ++ filenameRegex ++ ")+$"
+	,"^Output written on " ++ filenameRegex ++ " \\([[:digit:]]+ pages, [[:digit:]]+ bytes\\)\\.$"
+	] where
+	statistics =
+		["strings"
+		,"string characters"
+		,"words of memory"
+		,"multiletter control sequences"
+		,"words of font info for [[:digit:]]+ fonts,"
+		,"hyphenation exceptions"
+		,"PDF objects"
+		,"named destinations"
+		,"words of extra memory for PDF output"
+		]
 filenameRegex = "[-_./a-zA-Z0-9]*\\.[a-z]{2,}"
+ptRegex = "[[:digit:]]+(\\.[[:digit:]]+)?pt"
 
 matchBeginning pat_ = let pat = compile pat_ in \s ->
 	case match pat s of
@@ -64,10 +87,11 @@ matchBeginning pat_ = let pat = compile pat_ in \s ->
 		_ -> Nothing
 
 bracketNumber ss = (lines <$>) <$> bracketNumber' (unlines ss)
-bracketNumber' = matchBeginning "[[:space:]]*\\[[[:digit:]]+[[:space:]]*][[:space:]]*"
+bracketNumber' = matchBeginning ("[[:space:]]*\\[[[:digit:]]+([[:space:]]|[<>{}]|" ++ filenameRegex ++ ")*\\]")
 openFile  = matchBeginning ("[[:space:]]*\\(" ++ filenameRegex)
 closeFile = matchBeginning "[[:space:]]*\\)"
 beginMessage = matchBeginning "(LaTeX|Package) ([^ ]* )?(Info|Warning|Error): "
+beginHBox = matchBeginning ("(Over|Under)full \\\\hbox \\(((badness [[:digit:]]+)|(" ++ ptRegex ++ " too wide))\\) ")
 
 parseMessage b e ss = first (thisM:) (categorize' ss') where
 	(package, level) = case words b of
@@ -77,16 +101,24 @@ parseMessage b e ss = first (thisM:) (categorize' ss') where
 	ms        = map (dropWhile isSpace) (e:map (drop (length package + 2)) es)
 	thisM     = Message (read (init level)) package ms
 
+-- TODO: I'm sure " []" isn't the only thing that can follow an
+-- overfull/underfull hbox message; but what else can?
+parseHBox s (e:" []":ss) = first (HBox s e:) (categorize' ss)
+parseHBox s (e:ss      ) = first (HBox s e:) (categorize' ss)
+parseHBox s ([]        ) = ([HBox s hboxErrorTooShort], []) where
+	hboxErrorTooShort = "Huh. I was expecting another line to happen after this hbox error, but none did! Maybe there's a bug in the parser."
+
 categorize' [] = ([], [])
 categorize' (s:ss)
-	| any (`isPrefixOf` s) prefixes    = label (Known Boring)
-	| any (s==)            equalities  = label (Known Boring)
-	| any (`match` s)      regexen     = label (Known Boring)
-	| Just (f, s' ) <- openFile s      = let (b, e) = categorize' (s':ss)
-	                                     in first (file f b:) (categorize' e)
-	| Just (_, s' ) <- closeFile s     = ([], s':ss)
-	| Just (b, e  ) <- beginMessage s  = parseMessage b e ss
-	| Just (b, ss') <- bracketNumber (s:ss) = first (Known Boring b:) (categorize' ss')
+	| any (`isPrefixOf` s) prefixes         = label Boring
+	| any (s==)            equalities       = label Boring
+	| any (`match` s)      regexen          = label Boring
+	| Just (f, s' ) <- openFile s           = let (b, e) = categorize' (s':ss)
+	                                          in first (file f b:) (categorize' e)
+	| Just (_, s' ) <- closeFile s          = ([], s':ss)
+	| Just (b, e  ) <- beginMessage s       = parseMessage b e ss
+	| Just (b, ss') <- bracketNumber (s:ss) = first (Boring b:) (categorize' ss')
+	| Just _        <- beginHBox s          = parseHBox s ss
 	| otherwise = label Unknown
 	where
 	label f = first (f s:) (categorize' ss)
@@ -105,8 +137,9 @@ treeTake n = fst . treeTake' n where
 
 parse = categorize . coalesce
 interesting = concatMap locallyInteresting
-locallyInteresting (Known Boring s) = []
+locallyInteresting (Boring s) = []
 locallyInteresting (Message Info _ _) = []
+locallyInteresting (HBox {}) = []
 locallyInteresting (File f ls) = case interesting ls of
 	[] -> []
 	ls -> [File f ls]
