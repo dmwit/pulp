43f07ee7072875c4277fe12ea59251d9379180fc
Daniel Wagner <daniel@wagner-home.com>
2013-11-13 12:13:28 -0500

    take a stab at tracking line numbers when parsing log files

--- latex.hs
+++ latex.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE DataKinds, FlexibleInstances, GADTs, PatternGuards, StandaloneDeriving, TypeFamilies #-}
 
 import Control.Applicative
 import Control.Arrow
+import Control.Monad
 import Data.Char
 import Data.List
 import Text.Regex.Posix
+
+-- for auto-generated code
+import GHC.Show
 
 groupUntil = groupWhen . (not .)
 groupWhen p xs = case span p xs of
@@ -18,17 +22,71 @@ coalesce = map concat
 
 data MessageLevel = Info | Warning | Error
 	deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data Line
-	= Boring String
-	| HBox String String
-	| File String [Line]
-	| Message MessageLevel String [String]
-	-- TODO: add LineMarker Integer and emit an extra Line for each line that
-	-- matches "line [[:digit:]]+" or "lines [[:digit:]]+--[[:digit:]]+" or
-	-- something like that
-	| ExtraCloseFile
-	| Unknown String
-	deriving (Eq, Ord, Show, Read)
+data LineIndicators = Markers | Annotations
+	deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+type Annotation = (Maybe Integer, Maybe Integer)
+type family   File (a :: LineIndicators)
+type instance File Markers     = [Line Markers]
+type instance File Annotations = [(Annotation, Line Annotations)]
+
+data Line a where
+	Boring         :: String -> Line a
+	HBox           :: String -> String -> Line a
+	File           :: String -> File a -> Line a
+	Message        :: MessageLevel -> String -> [String] -> Line a
+	LineMarker     :: Integer -> Line Markers
+	ExtraCloseFile :: Line a
+	Unknown        :: String -> Line a
+deriving instance Eq   (Line Markers)
+deriving instance Ord  (Line Markers)
+deriving instance Show (Line Markers)
+deriving instance Read (Line Markers)
+-- auto-generated code {{{
+instance Show (Line Annotations) where
+  showsPrec a_a14L (Main.Boring b1_a14M)
+    = showParen
+        ((a_a14L >= 11))
+        ((.)
+           (showString "Boring ") (showsPrec 11 b1_a14M))
+  showsPrec a_a14N (Main.HBox b1_a14O b2_a14P)
+    = showParen
+        ((a_a14N >= 11))
+        ((.)
+           (showString "HBox ")
+           ((.)
+              (showsPrec 11 b1_a14O)
+              ((.) showSpace (showsPrec 11 b2_a14P))))
+  showsPrec a_a14Q (Main.File b1_a14R b2_a14S)
+    = showParen
+        ((a_a14Q >= 11))
+        ((.)
+           (showString "File ")
+           ((.)
+              (showsPrec 11 b1_a14R)
+              ((.) showSpace (showsPrec 11 b2_a14S))))
+  showsPrec a_a14T (Main.Message b1_a14U b2_a14V b3_a14W)
+    = showParen
+        ((a_a14T >= 11))
+        ((.)
+           (showString "Message ")
+           ((.)
+              (showsPrec 11 b1_a14U)
+              ((.)
+                 showSpace
+                 ((.)
+                    (showsPrec 11 b2_a14V)
+                    ((.)
+                       showSpace (showsPrec 11 b3_a14W))))))
+  showsPrec _ Main.ExtraCloseFile
+    = showString "ExtraCloseFile"
+  showsPrec a_a14Z (Main.Unknown b1_a150)
+    = showParen
+        ((a_a14Z >= 11))
+        ((.)
+           (showString "Unknown ") (showsPrec 11 b1_a150))
+  showList = showList__ (showsPrec 0)
+-- }}}
 
 compile :: String -> Regex
 compile = makeRegex
@@ -92,8 +150,17 @@ openFile  = matchBeginning ("[[:space:]]*\\(" ++ filenameRegex)
 closeFile = matchBeginning "[[:space:]]*\\)"
 beginMessage = matchBeginning "(LaTeX|Package) ([^ ]* )?(Info|Warning|Error): "
 beginHBox = matchBeginning ("(Over|Under)full \\\\hbox \\(((badness [[:digit:]]+)|(" ++ ptRegex ++ " too wide))\\) ")
+lineNumber = let pat = compile "lines? ([[:digit:]]+)(--([[:digit:]]+))?" in \s ->
+	case match pat s of
+		MR { mrSubList = [b, _, ""] } -> range b b
+		MR { mrSubList = [b, _, e ] } -> range b e
+		_ -> Nothing
+	where
+	convert = LineMarker . read
+	range s1 s2 = Just (convert s1, convert s2)
 
-parseMessage b e ss = first (thisM:) (categorize' ss') where
+-- TODO: don't silently drop l on the ground, and look for line markers within es
+parseMessage l b e ss = first (thisM:) (categorize' Nothing ss') where
 	(package, level) = case words b of
 		[_, package, level] -> (package, level)
 		[_, level] -> ("LaTeX", level)
@@ -103,30 +170,49 @@ parseMessage b e ss = first (thisM:) (categorize' ss') where
 
 -- TODO: I'm sure " []" isn't the only thing that can follow an
 -- overfull/underfull hbox message; but what else can?
-parseHBox s (e:" []":ss) = first (HBox s e:) (categorize' ss)
-parseHBox s (e:ss      ) = first (HBox s e:) (categorize' ss)
-parseHBox s ([]        ) = ([HBox s hboxErrorTooShort], []) where
-	hboxErrorTooShort = "Huh. I was expecting another line to happen after this hbox error, but none did! Maybe there's a bug in the parser."
+parseHBox l s ss = first (HBox s e:) (putLineHere l ss') where
+	(e, ss') = case break null ss of
+		(es, ss')
+			| null  es  -> (hboxErrorTooShort, ss')
+			| short es && last es == " []" -> (unlines (init es), ss')
+			| short es  -> (unlines es, ss')
+			| otherwise -> (hboxErrorTooLong, ss)
 
-categorize' [] = ([], [])
-categorize' (s:ss)
+	-- heuristic: the Overfull/Underfull hbox message is probably terminated by
+	-- a blank line the way we expect if the blank line comes within three or
+	-- four lines of the original complaint (otherwise guess that this compiler
+	-- uses a different format for all messages, or at least that this message
+	-- is in a different format)
+	short xs = zipWith const (replicate 5 ()) xs /= replicate 5 ()
+	hboxErrorTooShort = "Huh. I was expecting another line to happen after this hbox error, but none did! Maybe there's a bug in the parser."
+	hboxErrorTooLong  = "Huh. I was expecting this hbox error to end with a blank line pretty quickly, but it took a long time! Maybe there's a bug in the parser."
+
+maybeCons = maybe id (:)
+putLineHere l ss = first (maybeCons l) (categorize' Nothing ss)
+
+categorize' l [] = (maybeCons l [], [])
+categorize' l (s:ss)
 	| any (`isPrefixOf` s) prefixes         = label Boring
 	| any (s==)            equalities       = label Boring
 	| any (`match` s)      regexen          = label Boring
-	| Just (f, s' ) <- openFile s           = let (b, e) = categorize' (s':ss)
-	                                          in first (file f b:) (categorize' e)
-	| Just (_, s' ) <- closeFile s          = ([], s':ss)
-	| Just (b, e  ) <- beginMessage s       = parseMessage b e ss
-	| Just (b, ss') <- bracketNumber (s:ss) = first (Boring b:) (categorize' ss')
-	| Just _        <- beginHBox s          = parseHBox s ss
+	| Just (f, s' ) <- openFile s           = let (b, e) = categorize' Nothing (s':ss)
+	                                          in first (file f b:) (putLineHere l e)
+	| Just (_, s' ) <- closeFile s          = (maybeCons l [], s':ss)
+	| Just (b, ss') <- bracketNumber (s:ss) = first (Boring b:) (putLineHere l ss')
+	| (Nothing, Just (b, e)) <- (l, lineNumber s)
+	                                        = first (b:) (categorize' (Just e) (s:ss))
+	| Just (b, e  ) <- beginMessage s       = parseMessage l b e ss
+	| Just _        <- beginHBox s          = parseHBox l s ss
 	| otherwise = label Unknown
 	where
-	label f = first (f s:) (categorize' ss)
+	label f = first (f s:) (putLineHere l ss)
 	file = File . drop 1 . dropWhile isSpace
 
-categorize ss = let (b, e) = categorize' ss in b ++ case e of
+categorize ss = let (b, e) = categorize' Nothing ss in b ++ case e of
 	[] -> []
 	moreLines -> ExtraCloseFile : categorize moreLines
+
+parse = categorize . coalesce
 
 treeTake n = fst . treeTake' n where
 	treeTake' n _ | n <= 0 = ([], 0)
@@ -135,16 +221,31 @@ treeTake n = fst . treeTake' n where
 		(ls', n') -> first (File f ls':) (treeTake' n' rest)
 	treeTake' n (other:rest) = first (other:) (treeTake' (n-1) rest)
 
-parse = categorize . coalesce
+annotate :: File Markers -> File Annotations
+annotate = concatMap retag . liftA3 zip3 (scanl (flip combine) Nothing) (scanr combine Nothing) id where
+	combine (LineMarker n) l = Just n
+	combine _ l = l
+
+	retag (b, e, l) = (,) (b, e) <$> case l of
+		Boring s       -> [Boring s]
+		HBox s e       -> [HBox s e]
+		File s f       -> [File s (annotate f)]
+		Message l s ss -> [Message l s ss]
+		LineMarker n   -> []
+		ExtraCloseFile -> [ExtraCloseFile]
+		Unknown s      -> [Unknown s]
+
 interesting = concatMap locallyInteresting
-locallyInteresting (Boring s) = []
-locallyInteresting (Message Info _ _) = []
-locallyInteresting (HBox {}) = []
-locallyInteresting (File f ls) = case interesting ls of
+locallyInteresting (_, Boring s) = []
+locallyInteresting (_, Message Info _ _) = []
+locallyInteresting (_, HBox {}) = []
+locallyInteresting (l, File f ls) = case interesting ls of
 	[] -> []
-	ls -> [File f ls]
+	ls -> [(l, File f ls)]
 locallyInteresting other = [other]
+
+prettyPrint = show . interesting . annotate
 
 main = do
 	s <- readFile "dissertation/paper.log"
-	mapM_ print . treeTake 5 . interesting . parse $ s
+	putStrLn . prettyPrint . parse $ s
