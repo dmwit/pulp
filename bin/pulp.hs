563eaf330cf7ad4b34f849a7d3ce3ee6a5dd4c8e
Daniel Wagner <me@dmwit.com>
2015-03-20 11:37:00 -0700

    whitespace only

--- bin/pulp.hs
+++ bin/pulp.hs
@@ -27,37 +27,39 @@ data State = State { formula :: Config, inputSource :: InputSource } deriving (E
 defaultState = State defaultFormula STDIN
 
 usage = do
-  name <- getProgName
-  putStrLn $ "pulp v" ++ version
-  putStrLn $ "Usage: " ++ name ++ " [-f formula] [filename]\n"
-  putStrLn "[formula] specifies which messages to show; it is a boolean formula over message predicates:"
-  putStrLn $ intercalate "\n\t" [""
-                                  ,"overfull, underfull, hbox, vbox -- messages about over/underfull h/vboxes"
-                                  ,"info, message, warning, error   -- output from \\PackageError and friends"
-                                  ,"boring          -- matches a big regex for known, boring stuff"
-                                  ,"unknown         -- unparsed bits of the log file"
-                                  ,"close           -- TeX reported opening a file but not closing it, or closing more files than it opened"
-                                  ,"threshold <n>   -- *full *box messages whose badness/points are greater than n"
-                                  ,"package <regex> -- \\PackageError and friends was called by a package whose name matches the regex"
-                                  ,"details <regex> -- long output from \\PackageError or \\ClassError matches the regex"
-                                  ,"<regex>         -- a boring, unknown, info, message, warning, or error whose contents matches the regex"
-                                  ,"true            -- always"
-                                  ,"false           -- never"]
-  putStrLn $ "\nThe default formula is '" ++ defaultFormulaString ++ "'."
-  putStrLn "See https://github.com/dmwit/pulp for detailed info on formulas."
-  putStrLn $ "When processing a file 'file.log', if 'file.log.pulp' exists and contains\n" ++ "a formula, it overrides the '-f' option."
-  exitFailure
+	name <- getProgName
+	putStrLn $ "pulp v" ++ version
+	putStrLn $ "Usage: " ++ name ++ " [-f formula] [filename]\n"
+	putStrLn "[formula] specifies which messages to show; it is a boolean formula over message predicates:"
+	putStrLn $ intercalate "\n\t"
+		[""
+		,"overfull, underfull, hbox, vbox -- messages about over/underfull h/vboxes"
+		,"info, message, warning, error   -- output from \\PackageError and friends"
+		,"boring          -- matches a big regex for known, boring stuff"
+		,"unknown         -- unparsed bits of the log file"
+		,"close           -- TeX reported opening a file but not closing it, or closing more files than it opened"
+		,"threshold <n>   -- *full *box messages whose badness/points are greater than n"
+		,"package <regex> -- \\PackageError and friends was called by a package whose name matches the regex"
+		,"details <regex> -- long output from \\PackageError or \\ClassError matches the regex"
+		,"<regex>         -- a boring, unknown, info, message, warning, or error whose contents matches the regex"
+		,"true            -- always"
+		,"false           -- never"
+		]
+	putStrLn $ "\nThe default formula is '" ++ defaultFormulaString ++ "'."
+	putStrLn "See https://github.com/dmwit/pulp for detailed info on formulas."
+	putStrLn $ "When processing a file 'file.log', if 'file.log.pulp' exists and contains\n" ++ "a formula, it overrides the '-f' option."
+	exitFailure
 
 showVersion = do
-  putStrLn $ "pulp v" ++ version
-  exitFailure
+	putStrLn $ "pulp v" ++ version
+	exitFailure
 
 parseFormula expr s =
-  case Config.parse expr of
-    Left e -> do
-      putStrLn $ "Error while parsing formula '" ++ expr ++ "'\n\t" ++ intercalate "\n\t" e
-      exitFailure
-    Right f -> return $ s { formula = f }
+	case Config.parse expr of
+		Left e -> do
+			putStrLn $ "Error while parsing formula '" ++ expr ++ "'\n\t" ++ intercalate "\n\t" e
+			exitFailure
+		Right f -> return $ s { formula = f }
 
 parseArgs [] s = return s
 parseArgs ("-h":_) _ = usage
@@ -69,30 +71,30 @@ parseArgs ("-":args) s = parseArgs args $ s { inputSource = STDIN }
 parseArgs (file:args) s = parseArgs args $ s { inputSource = F file }
 
 checkForPulpFile s =
-  if inputSource s /= STDIN && formula s == defaultFormula
-  then do
-    -- TODO: should probably look at like .file.pulp rather than
-    -- file.pulp or something, but be careful about file names with
-    -- directory bits in
-    let F file = inputSource s
-    let pf = file ++ ".pulp"
-    f <- try (Config.parse <$> readFile pf)
-    case f of
-        Left e -> let e' :: IOException; e' = e in return s
-        Right (Right f) -> return $ s { formula = f }
-        Right (Left e) -> do
-          putStrLn $ "ERROR: bad pulp file 'pf':\n\t" ++ intercalate "\n\t" e
-          exitFailure
-  else return s
-	
+	if inputSource s /= STDIN && formula s == defaultFormula
+	then do
+		-- TODO: should probably look at like .file.pulp rather than
+		-- file.pulp or something, but be careful about file names with
+		-- directory bits in
+		let F file = inputSource s
+		let pf = file ++ ".pulp"
+		f <- try (Config.parse <$> readFile pf)
+		case f of
+			Left e -> let e' :: IOException; e' = e in return s
+			Right (Right f) -> return $ s { formula = f }
+			Right (Left e) -> do
+				putStrLn $ "ERROR: bad pulp file 'pf':\n\t" ++ intercalate "\n\t" e
+				exitFailure
+	else return s
+
 readInputSource STDIN = getContents
 readInputSource (F file) = readFile file
-       
+
 main = do
-  args <- getArgs
-  setLocaleEncoding latin1
-  -- TODO: we want a more complex chain, I think, so pull this out, refactor
-  -- it, think about the chain exactly, etc.
-  s <- parseArgs args defaultState >>= checkForPulpFile
-  log <- readInputSource $ inputSource s
-  putStr . prettyPrint . interesting (formula s) . Pulp.parse $ log
+	args <- getArgs
+	setLocaleEncoding latin1
+	-- TODO: we want a more complex chain, I think, so pull this out, refactor
+	-- it, think about the chain exactly, etc.
+	s <- parseArgs args defaultState >>= checkForPulpFile
+	log <- readInputSource $ inputSource s
+	putStr . prettyPrint . interesting (formula s) . Pulp.parse $ log
